f3662e51d635f1c3f7a063d4419cd9581dcd7f88
shiki-01 <0110shiki@gmail.com>
2024-04-24 02:04:03 +0900

    📁 save

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1>{$page.status} {$page.error?.message}</h1>
+<span style="font-size: 10em">
+    {#if $page.status === 404}
+        🤔
+    {:else}
+        😭
+    {/if}
+</span>
--- src/routes/pages/blogs/+page.server.ts
+++ src/routes/pages/blogs/+page.server.ts
@@ -1,6 +1,13 @@
 import { getList } from "$lib/microcms";
 import type { PageServerLoad } from "./$types";
 
+export const entries = () => {
+	return [
+		{ slug: 'hello-world' },
+		{ slug: 'another-blog-post' }
+	];
+};
+
 export const load: PageServerLoad = async () => {
   return await getList();
 };
